8040937a381c6d28a76d2807ac5a123ee375c19d
dhruv0206 <dhruv0128@gmail.com>
2023-11-19 18:49:39 -0500

    changes

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 import express from "express";
-import Hello from "./hello.js";
-import Lab5 from "./lab5.js";
+import Hello from "./hello";
+import Lab5 from "./lab5";
 import cors from "cors";
-import CourseRoutes from "./courses/routes.js";
-import ModuleRoutes from "./modules/routes.js";
+import CourseRoutes from "./courses/routes";
+import ModuleRoutes from "./modules/routes";
 import "dotenv/config";
 const app = express();
 app.listen(process.env.PORT || 4000);
